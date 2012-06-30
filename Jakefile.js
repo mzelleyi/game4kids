27662f7d8a92fa6abd0de7ee9dcea8775ce6f204
Marcelo Alaniz <alanizmarcelo@gmail.com>
2012-06-30 01:06:16 -0300

    refactoring all build structure

--- Jakefile.js
+++ Jakefile.js
@@ -1,6 +1,7 @@
 var util = require('util')
   , exec = require('child_process').exec
   , fs   = require('fs')
+  , fspath = require('path')
   , ugly = require('uglify-js')
   , jsp  = ugly.parser
   , pro  = ugly.uglify
@@ -11,12 +12,80 @@ var util = require('util')
   }
   ;
 
-
 var applications = [ 'alphabet', 'maze', 'memotest', 'qanda' ];
+
+var files = { 
+  alphabet: {
+    css: [
+      'css/styles.css'
+    ],
+    js: [
+      "js/utils.js",
+      "js/main.js", 
+      "js/sha256.js"
+    ],
+    images: [
+      "images/mnaranja.png",
+      "images/fondo.old.png",
+      "images/mazul.png",
+      "images/cartel.png",
+      "images/mvioleta.png",
+      "images/fondo.png",
+      "images/mrosa.png",
+      "images/bgmetal.jpg",
+      "images/logod.png",
+      "images/minsalud.png",
+      "images/fadestrip2.png",
+      "images/bg.gif",
+      "images/sound.png",
+      "images/close.png",
+      "images/win.png",
+      "images/background.png"
+    ],
+    sound: [
+      "sound/backsound.mp3",
+      "sound/01.wav",
+      "sound/suma_mal.wav",
+      "sound/12.wav",
+      "sound/osito.mp3",
+      "sound/11.wav",
+      "sound/01 (1).wav",
+      "sound/00.wav"
+    ]
+  }, 
+  maze: {
+    css: [
+      'styles.css'
+    ],
+    js: [
+      'util.js', 'main.js'
+    ],
+    images: [
+    ],
+    sound: [
+    ]
+  }, 
+  memotest: [
+  ], 
+  qanda: [
+  ], 
+};
+var src = 'src/';
+var build = 'build/';
 
 function log(message, color) {
   message = (typeof(color) === 'string')? colors[color] + message + colors.reset: message;
   process.stdout.write(message + '\n');
+}
+
+function mkdir(path, root) {
+  var dirs = path.split('/'), dir = dirs.shift(), root = (root||'')+dir+'/';
+  try { fs.mkdirSync(root); }
+  catch (e) {
+    //dir wasn't made, something went wrong
+    if(!fs.statSync(root).isDirectory()) throw new Error(e);
+  }
+  return !dirs.length||mkdir(dirs.join('/'), root);
 }
 
 task('deploy', [], function(params) {
@@ -27,9 +96,10 @@ task('deploy', [], function(params) {
 
 
 task('qanda', [], function(params) {
-  var path = applications[3] + '/src/'
-    , res  = applications[3] + '/js/'
+  var path = src + applications[3] + '/src/'
+    , res  = build + applications[3] + '/js/'
   ;
+
   fs.readdir(path, function(err, files) { 
     var filesjs = [], out, i, f;
     log('Building qanda javascript', 'red');
@@ -44,9 +114,11 @@ task('qanda', [], function(params) {
         out = pro.ast_mangle(out);
         out = pro.ast_squeeze(out);
         out = pro.gen_code(out)
-
-        log('\tMaking the result file in ' + res+f);
-        fs.writeFileSync(res + f, out, 'utf8')
+        fspath.exists(res, function(isthere) {
+          if (!isthere) { mkdir(res); }
+          fs.writeFileSync(res + applications[1] + '.js', out, 'utf8')
+          log('\tMaking the result file in ' + res+f);
+        });
       }
     }
 
@@ -58,8 +130,8 @@ task('qanda', [], function(params) {
 
 
 task('memotest', [], function(params) {
-  var path = applications[2] + '/src/'
-    , res  = applications[2] + '/js/'
+  var path = src + applications[2] + '/src/'
+    , res  = build + applications[2] + '/js/'
   ;
 
   log('Building memotest javascript', 'red');
@@ -80,17 +152,21 @@ task('memotest', [], function(params) {
     out = pro.ast_mangle(out);
     out = pro.ast_squeeze(out);
     out = pro.gen_code(out)
-
+    fspath.exists(res, function(isthere) {
+      if (!isthere) { mkdir(res); }
+      fs.writeFileSync(res + applications[1] + '.js', out, 'utf8')
     log('\tMaking the result file in ' + path+applications[2] + '.js');
-    fs.writeFileSync(res + applications[2] + '.js', out, 'utf8')
+    });
+
+
 
   });
 });
 
 
 task('maze', [], function(params) {
-  var path = applications[1] + '/src/'
-    , res  = applications[1] + '/js/'
+  var path = src + applications[1] + '/src/'
+    , res  = build + applications[1] + '/js/'
   ;
   fs.readdir(path, function(err, files) { 
     var filesjs = [], out, i;
@@ -112,15 +188,92 @@ task('maze', [], function(params) {
     out = pro.gen_code(out)
 
     log('\tMaking the result file in ' + path+applications[1] + '.js');
-    fs.writeFileSync(res + applications[1] + '.js', out, 'utf8')
+    fspath.exists(res, function(isthere) {
+      if (!isthere) { mkdir(res); }
+      fs.writeFileSync(res + applications[1] + '.js', out, 'utf8')
+    });
 
   });
 });
 
+
+function sync(o) {
+  var k, e;
+  if (typeof(o) === 'string') {
+    log(o);
+  } else {
+    for (k in o) {
+      e = o[k];
+      sync(e);
+    }
+  }
+}
 task('alphabet', [], function(params) {
-  var path = applications[0] + '/src/'
-    , res  = applications[0] + '/js/'
+  var listfiles
+    , out
+    , directory
+    , targetfile
+    , targetdirectory
   ;
+  
+  listfiles = files.alphabet.js;
+  directory = listfiles[0].split("/").slice(0, -1).join('/');
+  targetdirectory = build + 'alphabet/' + directory; 
+  outstream = listfiles.map(function(f){ 
+    return fs.readFileSync(src + 'alphabet/' + f, 'utf8');
+  });
+  outstream = outstream.join('\n');
+  outstream = jsp.parse(outstream);
+  outstream = pro.ast_mangle(outstream);
+  outstream = pro.ast_squeeze(outstream);
+  outstream = pro.gen_code(outstream)
+  fspath.exists(targetdirectory, function(isthere) {
+    targetfile = targetdirectory + '/alphabet.js';
+    if (!isthere) { mkdir(targetdirectory); }
+    fs.writeFileSync(targetfile, out, 'utf8');
+    log('\tMaking the result file in ' + targetfile );
+
+    listfiles = files.alphabet.css;
+    directory = listfiles[0].split("/").slice(0, -1).join('/');
+    targetdirectory = build + 'alphabet/' + directory; 
+    outstream = listfiles.map(function(f){ 
+      return fs.readFileSync(src + 'alphabet/' + f, 'utf8');
+    });
+    outstream = outstream.join('\n');
+    fspath.exists(targetdirectory, function(isthere) {
+      targetfile = targetdirectory + '/style.css';
+      if (!isthere) { mkdir(targetdirectory); }
+      fs.writeFileSync(targetfile, out, 'utf8');
+      log('\tMaking the result file in ' + targetfile );
+
+      listfiles = files.alphabet.images;
+      for (i=0; i<listfiles.length; i++) {
+        directory = listfiles[i].split("/").slice(0, -1).join('/');
+        targetdirectory = build + 'alphabet/' + directory; 
+        fspath.exists(targetdirectory, (function (i) {
+          return function(isthere) {
+            targetfile = build+ 'alphabet/' + listfiles[i];
+            sourcefile = src  + 'alphabet/' + listfiles[i];
+            if (!isthere) { mkdir(targetdirectory); }
+            console.log(sourcefile);
+            console.log(targetfile);
+
+            fs.readFileSync(sourcefile, 'utf8');
+            util.pump(sourcefile, targetfile);
+            log('\tMaking the result file in ' + targetfile );
+          }
+        })(i));
+      }
+
+    });
+
+  });
+
+
+  
+  
+
+/*
   fs.readdir(path, function(err, files) { 
     var filesjs = [], out, i;
 
@@ -139,11 +292,13 @@ task('alphabet', [], function(params) {
     out = pro.ast_mangle(out);
     out = pro.ast_squeeze(out);
     out = pro.gen_code(out)
-
+    fspath.exists(res, function(isthere) {
+      if (!isthere) { mkdir(res); }
+      fs.writeFileSync(res + applications[1] + '.js', out, 'utf8')
     log('\tMaking the result file in ' + path+applications[0] + '.js');
-    fs.writeFileSync(res + applications[0] + '.js', out, 'utf8')
-
+    });
   });
+*/
 });
 
 desc('Make maze game encription');
