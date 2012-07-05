ddd1ba5682f2419218a8e147253c5326a1741142
Marcelo Alaniz <alanizmarcelo@gmail.com>
2012-07-05 16:41:55 -0300

    fixing full screen on body

--- src/healthyteeth/js/main.js
+++ src/healthyteeth/js/main.js
@@ -1,103 +1,107 @@
-function __main__() {
-  var level = ('level' in qs)? qs.level: 'easy';
-  var points = 0;
-  var riddles = [];
-    
-  position = 0;
-  if (level === 'easy') {
-    riddles = [ 
-      {
-        question: "Cuando la pela y pela, mi madre en el dulce hogar parece telenovela: Como la hace llorar!",
-        options: [ 'Cebolla', 'Manzana', 'Naranja' ] ,
-        answer: 0 
-      },
-      {
-        question: "De la ensalada es la diosa, aunque tenga alguna arruga, quien crea que no es sabrosa, que pregunte a una tortuga.",
-        options: [ 'Lechuga', 'Achicoria', 'Tomate' ] ,
-        answer: 0 
-      },
-      {
-        question: "¿Es un repollo doctor? ¿O es una flor super star? ¿Es Verdura, acaso Flor o ambas cosas a la par?", 
-        options: [ 'Coliflor', 'Achicoria', 'Tomate' ] ,
-        answer: 0 
-      }
-    ];
-
-  } else if (level === 'middle') {
-    console.log("level not implemented");
-  } else {
-    console.log("level not implemented");
-  }
-
-
-
-  function make_question(q, callback) {
-    var o = $('render')
-      , message_question = document.createElement('p')
-      , buttons_wrapper = document.createElement('div')
-      , message_button 
+function __main__ () {
+  var Tablet = function()
+  {
+    var body = null
+      , that = this
+      , moveEvent = null
+      , offset = null
+        //if (e.clientY >= 50 && e.clientY <= 400 && e.clientX >= 270 && e.clientX <=1100) {
     ;
-
-    $('audio_11').play();
-    message_question.appendChild(document.createTextNode(q.question));
-
-    /* remove all childnodes and append the new one */
-    while (o.hasChildNodes()) { o.removeChild(o.firstChild); }
-    o.appendChild(message_question);
-
-    buttons_wrapper.id = 'buttonswrapper';
-    for (var i= 0; i< q.options.length; i++) {
-      message_button = document.createElement('a');
-      message_button.href = "javascript:void(0);";
-      message_button.appendChild(document.createTextNode(q.options[i]));
-      message_button.onclick = (function (index) {
-        return function(e) {
-          callback({ response: index, riddle: q } );
-        };
-      })(i);
-      buttons_wrapper.appendChild(message_button);
-    }
-    o.appendChild(buttons_wrapper);
-    /* end */
-
-  }
+    /* dom elements */
+    that.coins  = document.getElementById('coins');
+    that.tablet = document.getElementById('tablet');
+    that.teeths = document.createElement('div');
+    that.tablet.appendChild(that.teeths);   
 
 
-  function loop(event) {
-    event = (event) ? event: {};
-    event.response = ('response' in event)? event.response: 3;
-    event.riddle   = ('riddle'   in event)? event.riddle  : 'invalid';
 
-    if ((event.response !== 3) && (event.response !== event.riddle.answer)) {
-      $('audio_01').play();
-      ModalMessage.show("Tienes otro intento!");
-      points = (points === 0)? 0: points - 1;
-      return;
-    } else {
-      if (event.response === event.riddle.answer) {
-        points++;
-        // update points
+    /* dimensions */
+    that.size= { width : 900, height : 500 };
+    that.offset = getOffset(that.tablet);
+    that.limits = [
+      { x: that.offset.x                  , y: that.offset.y                     } , 
+      { x: that.offset.x + that.size.width, y: that.offset.y + that.size.height  } ];
+
+    that.center = { x: that.offset.x + (that.size.width/2), 
+                    y: that.offset.y + (that.size.height/2) };
+                      
+    
+    /* points */
+    that.good = 0;
+    that.bad = 0;
+
+
+    /* styling elements */
+    that.tablet.style.width  = String(that.size.width)  + 'px';
+    that.tablet.style.height = String(that.size.height) + 'px';
+
+    that.teeths.className  = 'pointer';
+    that.teeths.style.top  = that.center.y + 'px';
+    that.teeths.style.left = that.center.x + 'px';
+    console.log(that.center);
+
+    function meals() {
+      var div = document.createElement('div');
+      var timer = null;
+      var xpos  = 0;
+      var ypos  = 0;
+
+      xpos = getRandomInt(that.limits[0].x + 40, that.limits[1].x - 40);
+      ypos = that.limits[0].y;
+      that.tablet.appendChild(div);
+      if (xpos % 2 == 0) { 
+        div.className = "goodMeal"; 
+        div.style.backgroundImage = "url('images/goods/" + String(xpos%5) + ".png')"; 
+      } else { 
+        div.className = "badMeal" ; 
+        div.style.backgroundImage = "url('images/bads/" + String(xpos%5) + ".png')"; 
       }
+      div.style.left = xpos + "px";
+      div.style.top = ypos + "px";;
+
+      timer = setInterval(function moveMeal() {
+        div.style.top = ypos + "px";
+        ypos +=10;
+        if (ypos === that.limits[1].y - 20) {
+          clearInterval(timer);
+          div.className = 'removed';
+        }
+      }, 100);
     }
 
-    if( position < riddles.length ) {
-      make_question(riddles[position++], loop)
-    } else {
-      clock.stop();
-      ModalMessage.andRedirect(
-          "Terminaste!!! hiciste " + points + " puntos!!! <br>" +
-          "Y te sobraron " + clock.time + " segundos. Total de puntos: " +
-          String(clock.time + points)
-      , function() {
-        redirectTo('../index.html');
-        return;
+    that.doit = function() {
+      that.tablet = document.getElementById('tablet');
+      that.tablet.addEventListener('mousemove', function(e) {
+
+
+        if ((e.clientY >= (that.limits[0].y + 20) && e.clientY <= (that.limits[1].y -20 )) && 
+            (e.clientX >= (that.limits[0].x + 60) && e.clientX <= (that.limits[1].x +40 ) )) {
+          that.teeths.style.top = -20 + e.clientY + "px";
+          that.teeths.style.left = -20 + e.clientX + "px";
+        }
+        if (e.target.className === 'badMeal') {
+          e.target.className += ' eated';
+          setTimeout(function() {
+            e.target.className = 'removed';
+          }, 1000);
+ 
+          that.bad++;
+          that.coins.innerHTML = String(that.bad) + " --- " + String(that.good);
+        } else if (e.target.className === 'goodMeal') {
+          e.target.className += ' eated';
+          setTimeout(function() {
+            e.target.className = 'removed';
+          }, 1000);
+          that.good++;
+          that.coins.innerHTML = String(that.bad) + " --- " + String(that.good);
+        }
+
       });
-    }
 
-
-
+      setInterval(meals, 1000);
+    };
   }
-  loop(); 
+
+  t = new Tablet();
+  t.doit();
 }
-
-
--- src/libs/utils.js
+++ src/libs/utils.js
@@ -113,6 +113,42 @@ function removeClass(ele,cls) {
   }
 }
 
+function getOffset(e) {
+  var offset = {x:0,y:0};
+  e = (typeof(e) === 'string')? document.getElementById(e): e;
+  while (e)
+  {
+    offset.x += e.offsetLeft;
+    offset.y += e.offsetTop;
+    e = e.offsetParent;
+  }
+
+  /*
+  if (document.documentElement && (document.documentElement.scrollTop || document.documentElement.scrollLeft))
+  {
+    offset.x -= document.documentElement.scrollLeft;
+    offset.y -= document.documentElement.scrollTop;
+  }
+  else if (document.body && (document.body.scrollTop || document.body.scrollLeft))
+  {
+    offset.x -= document.body.scrollLeft;
+    offset.y -= document.body.scrollTop;
+  }
+  else if (window.pageXOffset || window.pageYOffset)
+  {
+    offset.x -= window.pageXOffset;
+    offset.y -= window.pageYOffset;
+  }
+  */
+
+  return offset;
+}
+
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 
 var ModalMessage  = {
   show: function(msg) {
