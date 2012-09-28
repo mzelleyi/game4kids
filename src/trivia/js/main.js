ae3133fe2963db60ee573bc5f81a2f8a2f15879e
Marcelo Alaniz <alanizmarcelo@gmail.com>
2012-09-28 15:18:02 -0300

    riddles in other file

--- src/trivia/js/main.js
+++ src/trivia/js/main.js
@@ -9,76 +9,9 @@ setInterval(function() {
 function __main__() {
   var level = ('level' in qs)? qs.level: 'easy';
   var points = 0;
-  var riddles = [];
   var positions = [];
     
-  if (level === 'easy') {
-    riddles = [
-    {
-      question: 'El Ministerio de la Vivienda organizó en julio los encuentros "Conociendo a mi vecino". Uno de los objetivos era:',
-      options: [ 'Que se conozcan los vecinos que antes de Navidad estarán en su casa propia.', 'Que se conozcan los vecinos que ya están viviendo en su casa propia.', 'Que se presenten los 50.000 vecinos al Gobernador Poggi.' ] ,
-      answer: 0
-    },
-    {
-      question: "La política habitacional de San Luis ostenta un récord en la República Argentina,",
-      options: [ 'Se construyeron 120.000 casas con el Ministerio de Vivienda', 'Es que una de cada dos familias vive en una casa construida por el Estado Provincial.', 'Las casas construidas son las mas grandes del país.' ] ,
-      answer: 1
-    },
-    {
-      question: "El costo de escriturar tu casa y hacerte Dueño es:",
-      options: [ 'Como máximo $800 en cuotas de $100.', '$2.500 en tres cuotas de $500', 'De el 3% del valor de la casa, aproximadamente $3.000' ] ,
-      answer: 0
-    },
-    {
-      question: "El Gobernador Claudio POGGI entrega viviendas",
-      options: [ 'Aun no entrega viviendas, comenzará en Diciembre del 2012', 'Todos los meses.', 'Dos viviendas cada tres meses.' ] ,
-      answer: 1
-    },
-    {
-      question: "El acceso a una vivienda digna es:",
-      options: [ 'Un lujo de quien pueda tenerla.', 'Es imposible en San Luis', 'Es un derecho de quien la necesita' ] ,
-      answer: 2
-    },
-    {
-      question: "El Gobierno de la Provincia de San Luis construyó y entregó",
-      options: [ 'Más de 55.000 viviendas', '1.500 viviendas', 'Un poco más de 10.000 viviendas' ] ,
-      answer: 0
-    },
-    {
-      question: "En la actualidad hay en marcha de construcción",
-      options: [ 'Aproximadamente 146 viviendas sociales', 'Aproximadamente 1000 viviendas sociales', 'Aproximadamente 2000 viviendas sociales' ] ,
-      answer: 2
-    },
-    {
-      question: "A pesar de ser llamadas ‘Viviendas Sociales’, las casas que construye el Estado Provincial son realizadas con material de primera calidad, poseen tres ambientes",
-      options: [ 'No, se construyen con materiales reciclados y económicos', 'No, solo poseen un ambiente amplio', 'Si, los materiales son de primera calidad y poseen tres ambientes' ] ,
-      answer: 2
-    },
-    {
-      question: "Siguiendo las políticas del Gobierno Provincial se están construyendo viviendas en toda la Provincia",
-      options: [ 'No, solo se construyen en la ciudad de San Luis', 'Se construyen en todo el territorio provincial', 'Se construyen en la ciudad de San Luis, Merlo y Villa Mercedes' ] ,
-      answer: 1
-    },
-    {
-      question: "Internos del servicio penitenciario realizarán materiales para el Plan Solidaridad",
-      options: [ 'Realizarán puertas y ventanas de maderas y metálicas para el Plan de viviendas.', 'Los internos no pueden tener ningún tipo de vínculo con la sociedad', 'Todos los internos trabajarán en las obras de construcción de viviendas.' ] ,
-      answer: 0
-    }
-  
-  
-  
-  
-  
-   
-    ];
-
   position = Math.floor(Math.random() * (riddles.length - 1));
-  } else if (level === 'middle') {
-    // middle
-    riddles = [];
-  } else {
-    riddles = [];
-  }
 
   function shuffle(list) {
     var i, j, t;
@@ -174,7 +107,7 @@ function __main__() {
     $('previous').pause();
     $('music').play();
  
-    clock.time = 20;
+    clock.time = 50;
     clock.start();
     loop();
     ModalMessage.close();
